d1016bf2a52cd78f959159f1b541c4264554c71e
Rajarshi Das <rd@rajarshi.me>
2024-05-03 16:09:35 +0530

    wip

--- products-add.php
+++ products-add.php
@@ -10,4 +10,20 @@ $query = "INSERT INTO `cfms`.`product` (`name`, `description`, `price`) VALUES (
 
 $mysqli->query($query);
 ?>
-Added <?= $name ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+    <link href='style.css' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+    <div class="contentbox">
+    Added <?= $name ?>
+    </div>
+</body>
